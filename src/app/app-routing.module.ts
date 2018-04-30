1fbdfbf0d604fb8ed7186eb5a378b50d3c0becbe
Nikhil12894 <nikhil12894@gmail.com>
2018-04-30 23:01:57 +0530

    gk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {path: 'nikhil12894.github.io/gk.io', redirectTo: '/home'},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
